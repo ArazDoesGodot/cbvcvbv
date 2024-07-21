09d262666e114902a245cd2c9ebb0d3af924052e
ArazDoesGodot <araznebiza@gmail.com>
2024-07-21 14:10:52 +0400

    hahahahaa

--- Node2D.gd
+++ Node2D.gd
@@ -8,3 +8,6 @@ var enet  = NetworkedMultiplayerENet.new()
 func _ready():
 	enet.create_server(2300)
 	get_tree().network_peer = enet
+
+func _physics_process(delta):
+	$Label.text = str(IP.get_local_addresses())
--- client.gd
+++ client.gd
@@ -4,8 +4,10 @@ extends Node2D
 
 var enet = NetworkedMultiplayerENet.new()
 
+var web = WebRTCMultiplayer.new()
+
 func _ready():
-	enet.create_client("www.arazdoesgithub.io",2300)
+	enet.create_client("2606:50c0:8000::153",2300)
 	get_tree().network_peer = enet
 	enet.connect("connection_succeeded",self,"waba")
 
